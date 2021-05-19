28f0a89f6423bdeb220ae113b804bba913dff2f7
dlrudfh <rudfh5256@naver.com>
2021-05-19 19:24:50 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./components/Header";
+
+function App() {
+  const city = "Seoul";
+  return (
+    <div className="App">
+      <Header region = "KR" cityName={city}/>
+    </div>
+  );
+}
+
+export default App;
