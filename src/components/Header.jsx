84129f5f749ea1c499080b8f89b0e502bf81df56
dlrudfh <rudfh5256@naver.com>
2021-05-19 19:33:02 +0900

    기본 코드 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,25 @@
-import logo from '../logo.svg';
+import logo from "../logo.svg";
 
 function Header(props) {
-	//const city = "Seoul";
+  // const city = "Seoul";
 
-	console.log(props)
-	const myCity = props.cityName;
-	return (
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-	        	<p>µµΩ√¥¬ : {myCity} </p>
-	        	<a
-	          		className="App-link"
-	            		href="https://reactjs.org"
-	            		target="_blank"
-	            		rel="noopener noreferrer"
-	          	>
-	            		Learn React
-	          	</a>
-	        </header>
-	);
+  console.log(props);
+  const myCity = props.cityName;
+
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>ÎèÑÏãúÎäî: {myCity} </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
 }
 
 export default Header;
